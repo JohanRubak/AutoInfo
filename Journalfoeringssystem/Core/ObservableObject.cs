12ddfee9e33074c4456af6ca5016003c54c566c8
Johan Rubak <202007752@post.au.dk>
2022-08-11 22:09:37 +0200

    Tilføjet opdateringsmuligheder af views

--- Journalfoeringssystem/Core/ObservableObject.cs
+++ Journalfoeringssystem/Core/ObservableObject.cs
@@ -9,7 +9,7 @@ using Journalfoeringssystem.Annotations;
 
 namespace Journalfoeringssystem.Core
 {
-   class ObservableObject: INotifyPropertyChanged
+   public class ObservableObject: INotifyPropertyChanged
    {
       public event PropertyChangedEventHandler PropertyChanged;
 
--- Journalfoeringssystem/MVVM/ViewModel/GeneratePDFViewModel.cs
+++ Journalfoeringssystem/MVVM/ViewModel/GeneratePDFViewModel.cs
@@ -3,10 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Journalfoeringssystem.Core;
 
 namespace Journalfoeringssystem.MVVM.ViewModel
 {
-   class GeneratePDFViewModel
+   public class GeneratePDFViewModel: ObservableObject
    {
+      public RelayCommand SearchCommand { get; set; }
+
+      private string _CPRnumber;
+
+      public string CPRNumber
+      {
+         get
+         {
+            return _CPRnumber;
+         }
+         set
+         {
+            _CPRnumber = value;
+            OnPropertyChanged(nameof(CPRNumber));
+         }
+      }
+
+      private string _name;
+
+      public string Name
+      {
+         get
+         {
+            return _name;
+         }
+
+         set
+         {
+            _name = value;
+            OnPropertyChanged(nameof(Name));
+         }
+      }
+
+      public GeneratePDFViewModel()
+      {
+         SearchCommand = new RelayCommand(o =>
+         {
+            Name = CPRNumber;
+         });
+      }
    }
 }
