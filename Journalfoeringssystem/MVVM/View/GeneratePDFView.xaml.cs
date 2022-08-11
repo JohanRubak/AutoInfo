98ea0f67d7a339a5f8375d4040b24d6d37b8f19b
Johan Andreas Balle Rubak <au662213@uni.au.dk>
2022-08-11 15:53:31 +0200

    Tilføjet views og viewmodels

--- Journalfoeringssystem/MVVM/View/GeneratePDFView.xaml.cs
+++ Journalfoeringssystem/MVVM/View/GeneratePDFView.xaml.cs
@@ -13,21 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Journalfoeringssystem
+namespace Journalfoeringssystem.MVVM.View
 {
    /// <summary>
-   /// Interaction logic for MainWindow.xaml
+   /// Interaction logic for GeneratePDFView.xaml
    /// </summary>
-   public partial class MainWindow : Window
+   public partial class GeneratePDFView : UserControl
    {
-      public MainWindow()
+      public GeneratePDFView()
       {
          InitializeComponent();
-      }
-
-      private void Button_Click(object sender, RoutedEventArgs e)
-      {
-         this.Close();
       }
    }
 }
--- Journalfoeringssystem/MVVM/ViewModel/MainViewModel.cs
+++ Journalfoeringssystem/MVVM/ViewModel/MainViewModel.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Journalfoeringssystem.Core;
 
 namespace Journalfoeringssystem.MVVM.ViewModel
 {
-   class MainViewModel
+   class MainViewModel: ObservableObject
    {
+      public HomeViewModel HomeVM { get; set; }
+
+      public GeneratePDFViewModel GeneratePDFVM { get; set; }
+
+      public RelayCommand HomeViewCommand { get; set; }
+      public RelayCommand GeneratePDFViewCommand { get; set; }
+      
+      private object _currentView;
+
+      public object CurrentView
+      {
+         get { return _currentView; }
+         set
+         {
+            _currentView = value; 
+            OnPropertyChanged();
+         }
+      }
+
+      public MainViewModel()
+      {
+         HomeVM = new HomeViewModel();
+         GeneratePDFVM = new GeneratePDFViewModel();
+         CurrentView = HomeVM;
+
+         HomeViewCommand = new RelayCommand(o =>
+         {
+            CurrentView = HomeVM;
+         });
+
+         GeneratePDFViewCommand = new RelayCommand(o =>
+         {
+            CurrentView = GeneratePDFVM;
+         });
+      }
    }
 }
